f601a880624f084e3364efa107761d8dcb233640
Soumya1-DS <soumyadipkhatua97@gmail.com>
2024-10-27 22:38:53 -0700

    Update 14.selection_sort.c

--- 14.selection_sort.c
+++ 14.selection_sort.c
@@ -1,47 +1,50 @@
 #include<stdio.h>
-#include<conio.h>
+
+void selection_sort(int [], int);
+int min(int [], int, int);
 
 int main()
 {
-    void selection_sort(int [], int);
-    int min(int [], int, int);
-    int a[10],i,N;
-    N=4;
-    printf("\nenter the %d element to sort :\n",N);
-    for(i=0;i<N;i++)
-	scanf("%d",&a[i]);
-    selection_sort(a,N);
-    printf("\n\nthe sorted list is :\n");
-    for(i=0;i<N;i++)
-    printf("%d ",a[i]);
-    getch();
+    int a[10], i, N;
+    N = 5;
+    printf("\nEnter %d elements to sort:\n", N);
+    for(i = 0; i < N; i++)
+        scanf("%d", &a[i]);
+
+    selection_sort(a, N);
+
+    printf("\n\nThe sorted list is:\n");
+    for(i = 0; i < N; i++)
+        printf("%d ", a[i]);
+
     return 0;
 }
 
-void selection_sort (int *array, int N)
+void selection_sort(int *array, int N)
 {
-    int i=0,loc=0,temp=0;
-    for(i=0;i<N;i++)
+    int i, loc, temp;
+    for(i = 0; i < N - 1; i++) 
     {
-        loc=min(array,i,N);
-        temp=array[loc];
-        array[loc]=array[i];
-        array[i]=temp;
-        printf("\n\nthe partial sorted list is :\n");
-        for(i=0;i<N;i++)
-        printf("%d ",array[i]);
+        loc = min(array, i, N);
+        printf("\nStep %d: Minimum value found is %d at index %d", i+1, array[loc], loc);
+
+        temp = array[loc];
+        array[loc] = array[i];
+        array[i] = temp;
+
+        printf("\n\nThe partially sorted list is:\n");
+        for(int j = 0; j < N; j++) 
+            printf("%d ", array[j]);   
     }
 }
 
 int min(int *array, int low_bound, int up_bound)
 {
-    int min=low_bound;
-    while(low_bound<up_bound)
+    int min = low_bound;
+    for(int i = low_bound + 1; i < up_bound; i++) 
     {
-        if(array[low_bound]<array[min])
-	    min=low_bound;
-	    low_bound++;
+        if(array[i] < array[min])
+            min = i;
     }
-    printf("\n............. Min=%d", min);
     return min;
 }
